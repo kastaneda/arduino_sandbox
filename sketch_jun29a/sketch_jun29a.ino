54f8ab07d825462b7480ef031a15ef00eda210b3
Dmitry Kolesnikov <kastaneda@gmail.com>
2020-06-29 16:43:27 +0300

    Arynchronous sonar

--- sketch_jun29a/sketch_jun29a.ino
+++ sketch_jun29a/sketch_jun29a.ino
@@ -6,11 +6,22 @@
 // Buzzer TMB1205
 #define buzzerPin 13
 
+// Echo >= 85 ms means distance more than 1.5 m
+#define pulseInTimeout 85000
+
+// Do not beep on big distance
+#define silentWhenTooFar 700
+
+unsigned long lastBeep = 0, nextBeep = 1000000;
+
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
   pinMode(buzzerPin, OUTPUT);
   digitalWrite(buzzerPin, LOW);
+
+  // Serial.begin(9600); // debug
 }
 
 // Get sensor echo duration in microseconds
@@ -33,14 +44,24 @@ void beep() {
 
 // Desired delay between beeps (milliseconds), depending on sensor distance (microseconds)
 unsigned long delayBetweenBeeps(unsigned long echoDuration) {
-  // Something very dumb: echoDuration / 8
-  return echoDuration >> 3;
+  return echoDuration / 10;
 }
 
 void loop() {
-  // TODO
-  // unsigned long next_beep;
+  unsigned long betweenBeeps;
 
-  delay(delayBetweenBeeps(measureEchoDuration()));
-  beep();
+  // Measure constantly
+  betweenBeeps = delayBetweenBeeps(measureEchoDuration());
+  // Serial.println(betweenBeeps); // debug
+  nextBeep = min(nextBeep, lastBeep + betweenBeeps);
+
+  if (millis() > nextBeep) {
+    // Beep, if it's  close enough
+    if ((nextBeep - lastBeep) < silentWhenTooFar) {
+      beep();
+    }
+
+    lastBeep = millis();
+    nextBeep = lastBeep + 1000000;
+  }
 }
