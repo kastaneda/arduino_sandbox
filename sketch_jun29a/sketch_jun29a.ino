b8f54e68adde66767ff1c0edf658c4116c9f92e9
Dmitry Kolesnikov <kastaneda@gmail.com>
2020-06-29 15:16:43 +0300

    update

--- sketch_jun29a/sketch_jun29a.ino
+++ sketch_jun29a/sketch_jun29a.ino
@@ -1,3 +1,6 @@
+// Based on
+// https://create.arduino.cc/projecthub/abdularbi17/ultrasonic-sensor-hc-sr04-with-arduino-tutorial-327ff6
+
 
 #define trigPin 9
 #define echoPin 10
@@ -12,11 +15,26 @@ void setup() {
 }
 
 void loop() {
-  // dummy
-  delay(1000);
+  long duration;
+  int distance;
   
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+
+  // Very dumb
+  delay(distance * 10);
+
   // beep
   digitalWrite(buzzerPin, HIGH);
-  delay(10);
+  delay(50);
   digitalWrite(buzzerPin, LOW);
 }
