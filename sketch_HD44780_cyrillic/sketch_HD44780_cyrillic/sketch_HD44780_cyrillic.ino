3cf6afcebdaf69267e5c28ea9551a7c3b3fbe648
Dmitry Kolesnikov <kastaneda@gmail.com>
2020-09-22 01:21:37 +0300

    update

--- sketch_HD44780_cyrillic/sketch_HD44780_cyrillic/sketch_HD44780_cyrillic.ino
+++ sketch_HD44780_cyrillic/sketch_HD44780_cyrillic/sketch_HD44780_cyrillic.ino
@@ -1,6 +1,8 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+byte prevLine[16];
+byte nextLine[16];
 
 void beep() {
   digitalWrite(A0, LOW);
@@ -10,13 +12,47 @@ void beep() {
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("hello, world!");
+  Serial.begin(9600);
 
   pinMode(A0, OUTPUT);
   digitalWrite(A0, HIGH);
 
-  beep();
+  for (byte i = 0; i < 16; i++) {
+    prevLine[i] = 32;
+  }
 }
 
+
 void loop() {
+  byte i;
+
+  if (Serial.available()) {
+    delay(100);
+
+    for (i = 0; i < 16; i++) {
+      prevLine[i] = nextLine[i];
+      nextLine[i] = 32;
+    }
+
+    i = 0;
+    while (Serial.available() > 0) {
+      int c = Serial.read();
+      if (c == 10) {
+        i = 16;
+      }
+      if (i < 16) {
+        nextLine[i++] = c;
+      }
+    }
+
+    lcd.clear();
+    for (i = 0; i < 16; i++) {
+      lcd.write(prevLine[i]);
+    }
+
+    lcd.setCursor(0, 1);
+    for (i = 0; i < 16; i++) {
+      lcd.write(nextLine[i]);
+    }
+  }
 }
