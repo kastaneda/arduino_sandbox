dcc988f0bce6a3e52083d7ee96ff35065d4ac559
Dmitry Kolesnikov <kastaneda@gmail.com>
2021-01-08 07:29:03 +0200

    update

--- sketch_jan08a_softserial/sketch_jan08a_softserial.ino
+++ sketch_jan08a_softserial/sketch_jan08a_softserial.ino
@@ -5,14 +5,34 @@ SoftwareSerial mySerial(10, 11); // RX, TX
 void setup()  
 {
   Serial.begin(9600);
+  Serial.write("Hello UART\n");
+
+  // DAMN IT DOES NOT SEND "OK" RESPONSE TO MY "AT"!
+
   mySerial.begin(9600);
+  delay(250);
+  mySerial.write("AT+NAMEArduinoBluetooth\n\r");
 }
 
 void loop()
 {
   if (mySerial.available())
     Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
+
+  //if (Serial.available())
+  //  mySerial.write(Serial.read());
+
+  if (Serial.available()) {
+    delay(50); // wait a little
+    Serial.write("\n* * * Got input: ");
+    while(Serial.available()) {
+      byte c = Serial.read();
+      Serial.write(c);
+      mySerial.write(c);
+    }
+    Serial.write("\n");
+    mySerial.write("\n\r");
+  }
+
 }
 
