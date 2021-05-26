887deb2b0838f62c4caa09d9cc820acc72302497
Dmitry Kolesnikov <kastaneda@gmail.com>
2021-05-26 10:11:06 +0300

    Add A5 sensor, change delay to 10s

--- sketch_may14a/sketch_may14a.ino
+++ sketch_may14a/sketch_may14a.ino
@@ -3,11 +3,12 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("INSERT INTO sensor (board_id, A0, A1, A2, millis) VALUES (1, ");
+  Serial.print("INSERT INTO sensor (board_id, A0, A1, A2, A5, millis) VALUES (1, ");
   Serial.print(analogRead(A0)); Serial.print(", ");
   Serial.print(analogRead(A1)); Serial.print(", ");
   Serial.print(analogRead(A2)); Serial.print(", ");
+  Serial.print(analogRead(A5)); Serial.print(", ");
   Serial.print(millis());
   Serial.println(");");
-  delay(1000);
+  delay(10000);
 }
