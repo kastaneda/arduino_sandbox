e77b858d2739366914b4fe23ffdc431d821a0299
Dmitry Kolesnikov <kastaneda@gmail.com>
2021-06-30 17:50:33 +0300

    update

--- sketch_may14a/sketch_may14a.ino
+++ sketch_may14a/sketch_may14a.ino
@@ -3,11 +3,14 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("INSERT INTO sensor (board_id, A0, A1, A2, A5, millis) VALUES (1, ");
+  Serial.print("INSERT INTO sensor (board_id, A0, A1, A2, A4, A5, A6, A7, millis) VALUES (2, ");
   Serial.print(analogRead(A0)); Serial.print(", ");
   Serial.print(analogRead(A1)); Serial.print(", ");
   Serial.print(analogRead(A2)); Serial.print(", ");
+  Serial.print(analogRead(A4)); Serial.print(", ");
   Serial.print(analogRead(A5)); Serial.print(", ");
+  Serial.print(analogRead(A6)); Serial.print(", ");
+  Serial.print(analogRead(A7)); Serial.print(", ");
   Serial.print(millis());
   Serial.println(");");
   delay(10000);
