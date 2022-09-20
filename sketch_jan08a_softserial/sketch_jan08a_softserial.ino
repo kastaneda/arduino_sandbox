0ac0c6bbfa143da12b9a251c5fdd6e32f94bd41b
Dmitry Kolesnikov <kastaneda@gmail.com>
2022-09-20 18:44:37 +0300

    Update to new proper scheme

--- sketch_jan08a_softserial/sketch_jan08a_softserial.ino
+++ sketch_jan08a_softserial/sketch_jan08a_softserial.ino
@@ -5,22 +5,27 @@ SoftwareSerial mySerial(10, 11); // RX, TX
 void setup()  
 {
   Serial.begin(9600);
-  Serial.write("Hello UART\n");
+  Serial.write("* * * Hello UART\n");
 
   // DAMN IT DOES NOT SEND "OK" RESPONSE TO MY "AT"!
 
   mySerial.begin(9600);
   delay(250);
   mySerial.write("AT+NAMEArduinoBluetooth\n\r");
+  // OKsetname
 }
 
 void loop()
 {
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-
-  //if (Serial.available())
-  //  mySerial.write(Serial.read());
+  if (mySerial.available()) {
+    delay(50);
+    Serial.write("\n* * * Responce: ");
+    while(mySerial.available()) {
+      byte c = mySerial.read();
+      Serial.write(c);
+    }
+    Serial.write("\n");
+  }
 
   if (Serial.available()) {
     delay(50); // wait a little
@@ -33,6 +38,4 @@ void loop()
     Serial.write("\n");
     mySerial.write("\n\r");
   }
-
 }
-
