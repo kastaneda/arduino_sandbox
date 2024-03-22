d032db71c7b56c3abb9fc50f17f3fe77a54b0803
Dmytro Kolesnykov <kastaneda@gmail.com>
2024-03-22 14:35:51 +0200

    hmm

--- silly_scheduler_ptr/ptr_ptr/test.c
+++ silly_scheduler_ptr/ptr_ptr/test.c
@@ -1,37 +1,28 @@
 #include <stdio.h>
 
-struct fn_sequence {
-  // scheduling infomration?
-  // delay before specific step?
-  struct fn_sequence* (*func)();
-};
+typedef void* (*recursion_t)();
 
-struct fn_sequence* turn_LED_on();
-struct fn_sequence* turn_LED_off();
+recursion_t turn_LED_on();
+recursion_t turn_LED_off();
 
-struct fn_sequence endless_blinking_led[2] = {
-  { turn_LED_on },
-  { turn_LED_off }
-};
-
-struct fn_sequence* turn_LED_on() {
+recursion_t turn_LED_on() {
   printf("digitalWrite(LED_BUILTIN, HIGH);");
-  return &endless_blinking_led[1];
+  return *turn_LED_off;
 }
 
-struct fn_sequence* turn_LED_off() {
+recursion_t turn_LED_off() {
   static int count = 5;
   printf("digitalWrite(LED_BUILTIN, LOW);");
-  return (--count) ? &endless_blinking_led[0] : NULL;
+  return (--count) ? *turn_LED_on : NULL;
 }
 
 int main() {
-  struct fn_sequence* seq_step = &endless_blinking_led[0];
+  recursion_t step = turn_LED_on;
 
   for (int t = 0; t < 15; t++) {
     printf("t=%02d: ", t);
-    if (seq_step) {
-      seq_step = seq_step->func();
+    if (step) {
+      step = step();
     } else {
       printf("(null)");
     }
