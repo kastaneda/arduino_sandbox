76310b079fb90e018c098ac6bf78a83a310213ed
Dmytro Kolesnykov <kastaneda@gmail.com>
2025-08-13 19:38:56 +0300

    print some debug info

--- hw/single_reset.py
+++ hw/single_reset.py
@@ -9,13 +9,36 @@ led = machine.Pin(2, machine.Pin.OUT)
 # and also give us some time to ensure
 # that the button does not restart the MCU yet
 for c in range(9):
+    print(led, 'value =', led.value())
     time.sleep_ms(250)
     led.value(1 - led.value())
+
+print(led, 'value =', led.value())
 # why is there 9 state changes, not 10?
 # bacause the LED is inverted and it's initial value is 0
 # sooo, we end the loop with 1, meaning the LED is off
 
+# or do it fancy
+#p = machine.PWM(led)
+#p.freq(10000)
+#for c in range(5):
+#    for d in range(1007, 256, -16):
+#        p.duty(d)
+#        time.sleep_ms(3)
+#    for d in range(256, 1023, 16):
+#        p.duty(d)
+#        time.sleep_ms(3)
+#p.deinit()
+#time.sleep_ms(5)
+#led.value(1)
+
 time.sleep_ms(500)
+
+# XXX DEBUG LOLWUT
+print('---------------------------------------')
+d1 = machine.Pin(5, machine.Pin.IN, machine.Pin.PULL_UP)
+print(d1, 'value =', d1.value())
+print('---------------------------------------')
 
 # PREPARE FOR WAKE-UP
 # discharge the RST capacitor by setting HIGH on both ends
@@ -31,6 +54,10 @@ time.sleep_ms(100)
 # after all that, set D1 to high-impendance mode with pull-up
 d1 = machine.Pin(5, machine.Pin.IN, machine.Pin.PULL_UP)
 time.sleep_ms(100)
+
+# XXX
+print(d1, 'value =', d1.value())
+print('---------------------------------------')
 
 # FINAL SHORT BLINK
 # just to debug / to show we're going to deep sleep
