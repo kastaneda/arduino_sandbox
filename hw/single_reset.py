fa1e76db0fd91d772a957dda51d0ca424f6e15bf
Dmytro Kolesnykov <kastaneda@gmail.com>
2025-08-13 14:45:10 +0300

    update

--- hw/single_reset.py
+++ hw/single_reset.py
@@ -1,20 +1,39 @@
-import machine, time
+import machine
+import time
 
+# GPIO 2 == pin D4 == built-in LED
+# the LED is inverted, HIGH = no light, LOW = bright blue
 led = machine.Pin(2, machine.Pin.OUT)
 
-for c in range(11):
-    led.value(1 - led.value())
+# BLINK EXACTLY FIVE TIMES
+# and also give us some time to ensure
+# that the button does not restart the MCU yet
+for c in range(9):
     time.sleep_ms(250)
+    led.value(1 - led.value())
+# why is there 9 state changes, not 10?
+# bacause the LED is inverted and it's initial value is 0
+# sooo, we end the loop with 1, meaning the LED is off
 
-# prepare for wake-up: discharge the RST capacitor
-d1 = machine.Pin(5, machine.Pin.OUT)
-d1.value(1) # now both capacitor legs is HIGH
 time.sleep_ms(500)
-# set D1 leg to high-impendance state
-d1 = machine.Pin(5, machine.Pin.IN, machine.Pin.PULL_UP)
-time.sleep_ms(50)
 
-# final short blink
+# PREPARE FOR WAKE-UP
+# discharge the RST capacitor by setting HIGH on both ends
+# initialize with high level on early configuration stage
+# GPIO 5 == pin D1
+d1 = machine.Pin(5, mode=machine.Pin.OUT, value=1)
+# otherwise, the next line of code, kinda 'd1.value(1)'
+# may not got a chance to run due to a sudden restart
+
+# give it some time (that's much more than it actually need)
+time.sleep_ms(100)
+
+# after all that, set D1 to high-impendance mode with pull-up
+d1 = machine.Pin(5, machine.Pin.IN, machine.Pin.PULL_UP)
+time.sleep_ms(100)
+
+# FINAL SHORT BLINK
+# just to debug / to show we're going to deep sleep
 led.value(0)
 time.sleep_ms(100)
 led.value(1)
